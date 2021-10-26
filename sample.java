f14d8a65b0fc905d637c40e402dff54d8ba028d4
LokiMesh <53658344+LokiMesh@users.noreply.github.com>
2021-10-26 09:55:59 +0530

    Add files via upload

--- sample.java
+++ sample.java
@@ -1,8 +1,11 @@
 public class SumOfNumbers
 {
     public static void main(String args[])
-    {
-        
-        System.out.println("Hi");
+    {  
+
+        int n1 = 225, n = 115, addition;
+        sum = n1 + n3;
+        System.out.println("The sum of numbers : " +addition);
+
     }
 }
